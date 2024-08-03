fc8eb56665ed3179aa16b620ee2da7d26ae52760
assafamosi123 <assafamosi@gmail.com>
2024-08-03 13:20:31 +0300

    update

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { addProduct, getProducts, deleteProduct, updateProductStock } = require('../controllers/productController');
+const { addProduct, getProducts, deleteProduct } = require('../controllers/productController');
+const { protect, admin } = require('../middlewares/authMiddleware');
+const upload = require('../utils/upload');
 
-router.route('/')
-    .get(getProducts)
-    .post(addProduct);
-
-router.route('/:id')
-    .delete(deleteProduct);
-
-router.route('/update-stock')
-    .put(updateProductStock);
+router.route('/').post(protect, admin, upload.array('images'), addProduct);
+router.route('/').get(getProducts);
+router.delete('/:id', protect, admin, deleteProduct);
 
 module.exports = router;
